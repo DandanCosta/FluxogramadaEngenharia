5f693c4ea421f7fc344e3b00b0c65bd7e00c4dbc
Danilo Costa <dandan_costa2222@outlook.com>
2025-05-13 16:47:56 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿char opçao = '\0';
+
+Console.WriteLine("--- Fluxograma da Engenharia ---");
+
+Console.Write("Está se movendo? (S/N): ");
+opçao = Convert.ToChar(Console.ReadLine()!);
+
+if (opçao == 'S' || opçao == 's'){
+        Console.Write("Deveria? (S/N): ");
+        opçao = Convert.ToChar(Console.ReadLine()!);
+        
+        if (opçao == 'S' || opçao == 's'){
+            Console.WriteLine("Otimo");
+        }
+        else if (opçao == 'N' || opçao == 'n'){
+            Console.WriteLine("Use Silver Tape.");
+        }
+
+}
+else if (opçao == 'N' || opçao == 'n'){
+        Console.Write("Deveria? (S/N): ");
+        opçao = Convert.ToChar(Console.ReadLine()!);
+
+        if (opçao == 'N' || opçao == 'n'){
+            Console.WriteLine("Otimo");
+        }
+        else if (opçao == 'S' || opçao == 's'){
+            Console.WriteLine("Use WD-40.");
+        }
+}
